bbf89a8bd3c7235d3340654badf68148074d8f40
Heather King <hkingdevelopment@gmail.com>
2021-11-22 18:36:26 +0000

    adds onChange functions to form

--- src/components/NewFilmForm.js
+++ src/components/NewFilmForm.js
@@ -1,16 +1,31 @@
-import React from "react";
+import React, {useState} from "react";
 
 const NewFilmForm = () => {
+
+    const [name, setName] = useState("");
+    const [url, setUrl] = useState("");
+
+    const handleNameChange = (event) => {
+        setName(event.target.value);
+    }
+
+    const handleUrlChange = (event) => {
+        setUrl(event.target.value);
+    }
 
     return (
         <form>
             <input 
                 type="text" 
                 placeholder="Film name"
+                value={name}
+                onChange={handleNameChange}
             />
             <input 
                 type="text" 
                 placeholder="IMDB url"
+                value={url}
+                onChange={handleUrlChange}
             />
             <input 
                 type="submit" 
